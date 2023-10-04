57d720f4a34e33e5e2cb9a051ba726ea45b1df40
majamalkiewicz <95423761+mayamalkiewicz@users.noreply.github.com>
2023-10-04 11:10:27 +0200

    Implement count elements in array

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,12 @@
 class CountElements
 
+  def initialize(array)
+    @array = array
+  end
+
+  def perform
+    @array.each_with_object(Hash.new(0)) do |element, hash|
+      hash[element] += 1 
+    end
+  end
 end
